a20a715ab8070dcb90ee7510bc51088cc0b96560
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-06-23 14:56:12 -0500

    Update app.php

--- practicabase/libs/app.php
+++ practicabase/libs/app.php
@@ -8,25 +8,28 @@ require_once 'controllers/errores.php';
            $url=rtrim($url,'/');
             $url=explode('/',$url); 
 
-            if(empty($url[0])){
+              if(empty($url[0])){
                 $archivoController='controllers/main.php';
                 require_once $archivoController;
                 $controller=new Main();
-                
+                $controller->render();
                 $controller->loadModel('main');
                 return false;
             }
 
-            $archivoController='controllers/'.$url[0].'.php';
+             $archivoController='controllers/'.$url[0].'.php';
             if(file_exists($archivoController)){
               require_once $archivoController;
                 $controller=new $url[0];
+               // $controller->render();
                 $controller->loadModel($url[0]);
                 if(isset($url[1])){
-                echo $url[1];  
-                echo $archivoController; 
-                //$controller->($url[1]);
-               }
+                    $controller->{$url[1]}();  
+                
+                }else{
+                    $controller->render();
+
+                }
                
             }
             else{
