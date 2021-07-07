1a198a290060b09f54c4da124a5d9404149ed757
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-07-07 15:05:58 -0500

    Actualizacion de action formulario

--- views/menú_Establecimiento.php
+++ views/menú_Establecimiento.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/inicio_establecimiento.php" type="text/css">
-    <link rel="stylesheet" href="public/css/cabecera.css" type="text/css">
-    <script type="text/javascript" src="./public/css/funciones.js"></script>
+     <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/inicio_establecimiento.php" type="text/css">
+    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/cabecera.css" type="text/css">
+    <script type="text/javascript" src="<?php echo constant('URL');?>public/css/funciones.js"></script>
 
 </head>
 <body class="inicial" id="pawegrap">
 <?php require 'views/header.php';?>
 
 <div id="content"> 
-    <img style="float:left;" src="./public/IMG/Comida.png" alt="descripción" />
+    <a href="<?php session_start(); echo constant('URL');?>home_establecimiento"><img style="float:left; width:50px" src="<?php echo constant('URL');?>public/IMG/flecha.png" /></a>
+
+    <img style="float:left;" src="<?php echo constant('URL');?>.upload/img/Web_fotoreceta.jpg" alt="descripción" />
     <article class="cuadro-inicial">
     <!--Los datos del formulario son enviados a ingresar.php ahi se usan para almacenarlos en la base de datos-->
-    <form action="Inicio_Establecimiento" method="post" id="cuadro" class="cuadro">
+    <form action="<?php echo constant('URL');?>nuevo/agregarPlatillo" method="post" id="cuadro" class="cuadro">
     <h1>Menú</h1>
 
     <p>Nombre del platillo:<input type="text" placeholder="Nombre del platillo" name="nombre" id="nombre"></p> 
