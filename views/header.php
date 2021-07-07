aa3f4c831f3f7d5b326996ed8fbac85ac6746fb2
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-07-07 09:17:59 -0500

    Actualizacion enlaces css

--- views/header.php
+++ views/header.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/login.css"  type="text/css">
+    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/login.css"  type="text/css">
 
     <title>Encabezado</title>
 </head>
 <body>
    <div id="header">
     <ul class="clearfix" >
-        <li><a href="main">Inicio</a></li>
+        <li><a href="<?php echo constant('URL');?>main">Inicio</a></li>
     </ul>
    </div>  
 </body>
