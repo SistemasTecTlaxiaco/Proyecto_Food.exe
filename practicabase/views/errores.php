658919685c2c4d0bc2ea4fe502d0889fd2026bd2
dulce-blm <dulce.belem0409@gmail.com>
2021-07-09 18:17:30 -0500

    Delete files

--- practicabase/views/errores.php
+++ practicabase/views/errores.php
@@ -4,15 +4,12 @@
         <meta charset="UTF-8">
         <title>MENSAJE ERROR</title>
         <link rel="stylesheet" href="public/css/login.css" type="text/css">
-   
     </head>
     <body>
     <?php require 'views/header.php';?>
         <div id="main">
             <h1 class="error"><?php echo $this->mensaje; ?></h1>
         </div>
-        <?php require 'views/footer.php';?>
-        
+        <?php require 'views/footer.php';?> 
    </body>
-
 </html>
