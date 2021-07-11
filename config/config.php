53c72f19502aeb3b55a9d7dd725c2214c8118ff9
Jeser Leonel Peña Nieto <79876847+JeserLeonel@users.noreply.github.com>
2021-07-11 17:14:19 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 //mantiene estructura sin importar que parte del documento se acceda
-define ('URL','https://food2exe.herokuapp.com/');
+const URL="https://food2exe.herokuapp.com/";
 //variables para la base de datos
-define ('HOST','pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define ('DB','	gdxzaewzup8sigpm');
-define ('USER','nj4v3bybsyrznops');
-define ('PASSWORD','kcaoyaygqjrt2ry9');
-define ('CHARSET','utf8mb4');
-define ('PUERTO','3306');
+define ('DB_HOST','pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define ('DB_NAME','gdxzaewzup8sigpm');
+define ('DB_USER','nj4v3bybsyrznops');
+define ('DB_PASSWORD','kcaoyaygqjrt2ry9');
+define ('DB_CHARSET','utf8');
+//define ('PUERTO','3306');
 ?>
