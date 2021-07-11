261ab7616d9b1b235a92beaa7c26810fe592617d
Brendalp <72048235+Brendalp@users.noreply.github.com>
2021-07-11 18:21:08 -0500

    Update home_establecimiento.php

--- views/home_establecimiento.php
+++ views/home_establecimiento.php
@@ -4,8 +4,6 @@ include_once("./models/nuevomodel.php");
 include_once("./libs/model.php");
 $this->db=new Database();
 $db = $this->db->connect();
-error_reporting(0);
-session_start();
 $idusuario=$_SESSION['id_establecimiento'];
 //echo $idusuario;
 $query= $db->prepare("SELECT * FROM establecimiento WHERE id_establecimiento='$idusuario'");
@@ -21,18 +19,22 @@ $row=$query->fetch(PDO::FETCH_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-      <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/login.css" type="text/css">
-    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/cabecera.css" type="text/css">
-    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/barra.css" type="text/css">
-    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/barra.js" type="text/css">
-   
+    <link rel="stylesheet" href="public/css/login.css" type="text/css">
+    <link rel="stylesheet" href="public/css/cabecera.css" type="text/css">
+    <link rel="stylesheet" href="public/css/barra.css" type="text/css">
+    <link rel="stylesheet" href="public/css/barra.js" type="text/css">
+    <link rel="stylesheet" href="../public/css/login.css" type="text/css">
+    <link rel="stylesheet" href="../public/css/cabecera.css" type="text/css">
+    <link rel="stylesheet" href="../public/css/barra.css" type="text/css">
+    <link rel="stylesheet" href="../public/css/barra.js" type="text/css">
+
 
     <title>HOME ESTABLECIMIENTO</title>
 </head>
 <body id="pawegrap" >
 <div id="header2">
     <ul class="clearfix" >
-        <li><a href="main.php"> &#x1F37D Food.exe</a></li>
+        <li><a href="views/main.php"> &#x1F37D Food.exe</a></li>
         
     </ul>
    </div>  
@@ -41,7 +43,11 @@ $row=$query->fetch(PDO::FETCH_ASSOC);
         <span>&#8803</span>
       </div>
       <ul>
-        <li ><a href="menu_Establecimiento.php" id="barra">Menu</a></li>
+        <li>
+        <!--  <img src="img/logo.jpg" alt="Logo Fazt" class="logo"> -->
+        </li>
+
+        <li ><a href="#" id="barra">Menu</a></li>
         <li></li>
         <li><a href="#" id="barra">Ofertas</a></li>
         <li></li>
@@ -52,15 +58,18 @@ $row=$query->fetch(PDO::FETCH_ASSOC);
         <li><a href="#" id="barra">Lista de pedidos</a></li>
         <li></li>
         <li><a href="#" id="barra">Mapa de establecimientos</a></li>
-        <li></li>
-        <li><a href="<?php echo constant('URL');?>cerrar" id="barra">Cerrar sesion</a></li>
-     
+
+
       </ul>
  </div>
- <img style="float:left;" src="<?php echo constant('URL');?>public/IMG/icono.png" alt="descripción" />
+ <img style="float:left;" src="../public/IMG/icono.png" alt="descripción" />
 
       <article class="cuadro-inicial">
     <h1 style="color: #fd9c28; text-align: left;">DATOS DEL ESTABLECIMIENTO</h1>
+    <?php
+                include_once 'models/establecimiento.php';  
+                $establecimiento=new Establecimiento();
+                ?>  
     <h2 style=" text-align: left;">            
     <strong> Nombre </strong> : <?php echo utf8_decode($row['nombre']);?><br><br><br>
     <strong> Telefono </strong> : <?php echo utf8_decode($row['telefono']);?><br><br><br>
@@ -73,7 +82,8 @@ $row=$query->fetch(PDO::FETCH_ASSOC);
    
     </article>
 
-    <script src="<?php echo constant('URL');?>public/css/barra.js"></script>
+    <script src="../../public/css/barra.js"></script>
 
 </body>
 </html>
+
