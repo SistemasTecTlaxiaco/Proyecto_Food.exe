7c901a758a59674648dac43ef4a07d69cc55aa77
Jeser Leonel Peña Nieto <79876847+JeserLeonel@users.noreply.github.com>
2021-07-11 14:24:19 -0500

    DB

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
 //mantiene estructura sin importar que parte del documento se acceda
-define ('URL','https://localhost/practicabase/');
+define ('URL','https://food2exe.herokuapp.com/');
 //variables para la base de datos
-define ('HOST','localhost');
-define ('DB','base de datos');
-define ('USER','root@localhost');
-define ('PASSWORD','');
+define ('HOST','pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define ('DB','	gdxzaewzup8sigpm');
+define ('USER','nj4v3bybsyrznops');
+define ('PASSWORD','kcaoyaygqjrt2ry9');
 define ('CHARSET','utf8mb4');
 define ('PUERTO','3306');
 ?>
