d05f83c861c0479b7a0a3a2aba2385d580d99c63
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-10-14 02:12:38 -0500

    Configuracion heroku

---
+++ establecimientos/config/config.php
@@ -0,0 +1,12 @@
+<?php
+//mantiene estructura sin importar que parte del documento se acceda
+//define ('URL','https://profood.herokuapp.com/');
+define ('URL','https://localhost/REPOSITORIO/proyecto/');
+//variables para la base de datos
+define ('HOST','localhost');
+define ('DB','base de datos');
+define ('USER','root@localhost');
+define ('PASSWORD','');
+define ('CHARSET','utf8mb4');
+define ('PUERTO','3307');
+?>
