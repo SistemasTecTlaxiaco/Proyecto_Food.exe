be019a88de3e3dac74d91bf310cd3feaed710045
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-10-14 03:40:38 -0500

    Modificacion de estilos y tabla

--- views/menú_Establecimiento.php
+++ views/menú_Establecimiento.php
@@ -4,35 +4,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/inicio_establecimiento.php" type="text/css">
-    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/cabecera.css" type="text/css">
-    <script type="text/javascript" src="<?php echo constant('URL');?>public/css/funciones_e.js"></script>
+     <meta http-equiv="Expires" content="0"> <meta http-equiv="Last-Modified" content="0"> 
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate"> 
+    <meta http-equiv="Pragma" content="no-cache"> 
+    <title>MENÚ</title>
+    <link rel="stylesheet" href="<?php echo constant('URL');?>public/CSS/estilos.css" type="text/css">
 
 </head>
-<body class="inicial" id="pawegrap">
+<body id="inicial">
 <?php require 'views/header.php';?>
+<img  id="i" src="<?php echo constant('URL');?>public/IMG/icono.png" alt="descripción" />
 
-<div id="content"> 
-    <a href="<?php session_start(); echo constant('URL');?>home_establecimiento"><img style="float:left; width:50px" src="<?php echo constant('URL');?>public/IMG/flecha.png" /></a>
-
-    <img style="float:left;" src="<?php echo constant('URL');?>.upload/img/Web_fotoreceta.jpg" alt="descripción" />
-    <article class="cuadro-inicial">
     <!--Los datos del formulario son enviados a ingresar.php ahi se usan para almacenarlos en la base de datos-->
-    <form action="<?php echo constant('URL');?>nuevo/agregarPlatillo" method="post" id="cuadro" class="cuadro">
-    <h1>Menú</h1>
+    <form action="<?php echo constant('URL');?>nuevo/agregarPlatillo" method="post" id="login" class="login">
+    <h3  id="titulo">PLATILLOS</h3>
 
-    <p>Nombre del platillo:<input type="text" placeholder="Nombre del platillo" name="nombre" id="nombre"></p> 
+    <div class="form-group">
+			<label>Nombre:</label>
+			<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Introduzca nombre de platillo">
+		</div> <br>
+        <div class="form-group">
+			<label>Descripción:</label>
+			<textarea class="form-control" name="cara" id="cara" placeholder="Introduzca breve descripción">
+</textarea> </div> <br>
+        <div class="form-group">
+			<label>Precio:</label>
+			<input type="number" class="form-control" name="precio" id="precio" >
+		</div> <br>
+        <div class="form-group">
+			<label>Imagen de muestra:</label>
+			<input type="file" class="form-control"name="imgplatillo" id="imgplatillo" >
+		</div> <br>
 
-    <p>Descripcion:<input type="text" placeholder="Ingresar descripción" name="caracteristicas" id="caracteristicas"></p>
-
-    <p>Precio:<input type="number" placeholder="Ingresar precio" name="precio" id="precio"></p> 
-
-    <p>Imagen de muestra:<input type="file" placeholder="Seleccione archivo" name="Imgplatillo" onchange="ValidarImagen(this);"></p> 
-
-    </p>       <br>
     <!--Al presionar el boton, se validan datos desde javascript y luego si son validos se almacenan en la base de datos-->
-    <input type="submit" value="Actualizar" id="platillos" class="menú_Establecimiento" onclick="menu_es()"></input>
+    <input type="submit" id="platillos" class="btn btn-primary btn-block"  value="ACTUALIZAR" onclick="menu_es()"></input>
+
 </form>
+    
     </article>
 </div>
 
