96b10b4710368f52d7daf51a2bfbe0d7cb115a65
dulce-blm <72100538+dulce-blm@users.noreply.github.com>
2021-10-18 12:47:18 -0500

    Update ofertas.php

--- establecimientos/views/ofertas.php
+++ establecimientos/views/ofertas.php
@@ -16,11 +16,14 @@
 
     <form id="login" name="login" action="" method="post" autocomplete="off">
 	
-    <div class="form-group">
-			<label>Nombre:</label>
-			<input type="text" class="form-control" id="nombrecompleto" name="nombrecompleto" placeholder="Introduzca nombre de platillo">
-			</div> <br>	
-	
+    <div class="form-group">	
+	<!--	<form id="login" name="login" action="upload.php" method="POST" enctype="multipart/form-data">
+					<input style="position:absolute; top:350px;left:200px;" type="file" name="file">
+					<p><img class="img5" src="<?php echo constant('URL');?>public/imagen/galeria.png" alt="mas"></p>
+					</form>-->
+					<img style="width:50px;"src="<?php echo constant('URL');?>public/imagen/mas.png" alt="mas">     
+					<input type="submit" id="platillos" class="btn btn-primary btn-block"  value="ACTUALIZAR" onclick="menu_es()"></input>
+                </form>
     <div class="form-group">
 			<label>Descripción:</label>
 			<textarea class="form-control" name="cara" id="cara" placeholder="Introduzca breve descripción">
