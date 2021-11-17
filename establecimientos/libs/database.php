ccc3d919f90b7ee84cb470658e7e72263dd9f3be
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-17 14:04:44 -0600

    Update database.php

--- establecimientos/libs/database.php
+++ establecimientos/libs/database.php
@@ -4,9 +4,9 @@ class Database{
     function connect(){
         try{
         //DATOS PARA CONEXION HEROKU
-        $dsn = 'mysql:host=wcwimj6zu5aaddlj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; port=3306; dbname=mmdfc754pwa3llx7;';
-        $username = 'zfkdp6whp5okxxgy';
-        $password = 'c95r118qp2h6ev0c'; 
+        $dsn = 'mysql:host=pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; port=3306; dbname=gdxzaewzup8sigpm;';
+        $username = 'nj4v3bybsyrznops';
+        $password = 'kcaoyaygqjrt2ry9'; 
         
         /*PARA PRUEBAS LOCALES
         $dsn = 'mysql:host=localhost; port=3306; dbname=food;';
