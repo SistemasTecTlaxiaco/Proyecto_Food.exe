76d12ff4a171bb00db2a49987505d7012ca2fff9
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-17 13:01:03 -0600

    detalle de platos

--- establecimientos/views/detallePlato_establecimiento.php
+++ establecimientos/views/detallePlato_establecimiento.php
@@ -8,7 +8,7 @@
     <meta http-equiv="Expires" content="0"> <meta http-equiv="Last-Modified" content="0"> 
     <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate"> 
     <meta http-equiv="Pragma" content="no-cache"> 
-    <title>MENÚ</title>
+    <title>DETALLE PLATILLO</title>
     <link rel="stylesheet" href="<?php echo constant('URL');?>public/CSS/estilos.css" type="text/css">
 
 </head>
@@ -16,26 +16,32 @@
 <?php require 'views/header.php';?>
 
 <img  id="i" src="<?php echo constant('URL');?>public/IMG/icono.png" alt="descripción" />
-
+<?php    
+    include_once 'models/platillo.php';
+    foreach ($this->platillo as $row){
+        $platillo=new Platillo();
+        $platillo=$row;
+        
+        ?>
     <!--Los datos del formulario son enviados a ingresar.php ahi se usan para almacenarlos en la base de datos-->
     <form action="<?php echo constant('URL');?>menu_gral/actualizarPlatillo" method="post" id="login" class="login">
     <h3 class="center">Detalle de <?php echo $this->platillo->nombre; ?> </h3>
 
     <div class="form-group">
 			<label>Platillo:</label>
-			<input type="text" class="form-control" disabled value="<?php echo $this->platillo->id_platillos; ?>" required>
+			<input type="text" name="id_platillos" id="id_platillos" class="form-control"  value="<?php echo $platillo->id_platillos; ?>" required>
 		</div> <br>
     <div class="form-group">
 			<label>Nombre:</label>
-			<input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo $this->platillo->nombre; ?>" required>
+			<input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo $platillo->nombre; ?>" required>
 		</div> <br>
+        
         <div class="form-group">
 			<label>Descripción:</label>
-			<textarea class="form-control" name="cara" id="cara" value="<?php echo $this->platillo->caracteristicas; ?>" required>
-</textarea> </div> <br>
+			<textarea class="form-control" name="caracteristicas" id="caracteristicas"required> <?php echo $platillo->caracteristicas; ?> </textarea> </div> <br>
         <div class="form-group">
 			<label>Precio:</label>
-			<input type="number" class="form-control" name="precio" id="precio" value="<?php echo $this->platillo->precio; ?>" required>
+			<input type="number" class="form-control" name="precio" id="precio" value="<?php echo $platillo->precio; ?>" required>
 		</div> <br>
         <!--
         <div class="form-group">
@@ -43,8 +49,10 @@
 			<input type="file" class="form-control"name="imgplatillo" id="imgplatillo" >
 		</div> <br>-->
 
+
     <!--Al presionar el boton, se validan datos desde javascript y luego si son validos se almacenan en la base de datos-->
     <input type="submit" id="platillos" class="btn btn-primary btn-block"  value="ACTUALIZAR PLATILLO" ></input>
+    <?php }?>
 
 </form>
     </article>
