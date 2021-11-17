e710238c103c40ef52ba33eba2e57b9173b0380e
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-17 15:27:35 -0600

    Update home_cliente.php

--- views/home_cliente.php
+++ views/home_cliente.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/login.css" type="text/css">
-    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/cabecera.css" type="text/css">
+    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/estilos.css" type="text/css">
 </head>
 <body class="mapa">
 <?php require 'views/header.php';?>
