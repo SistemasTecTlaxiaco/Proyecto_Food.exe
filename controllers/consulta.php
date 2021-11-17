f1523e8c8b2b16dced5b2e1a9ee1bac01b2f2e86
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-17 14:30:23 -0600

    Update consulta.php

--- controllers/consulta.php
+++ controllers/consulta.php
@@ -6,14 +6,10 @@
        {
            parent::__construct();
            $this->view->establecimiento=[];
-        //$this->view->datos=[];
-          // $this->view->render('consulta/index');
        }
        function render(){
            $establecimiento=$this->model->get();
            $this->view->establecimiento=$establecimiento;
-         //  $alumnos=$this->view->datos=$this->model->get();
-           
            $this->view->render('home_establecimiento');
        }
 
