4e656a0ced0ca28f06364ce95b224611e022e5e3
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-17 16:01:39 -0600

    Update app.php

--- libs/app.php
+++ libs/app.php
@@ -18,17 +18,25 @@ require_once 'controllers/errores.php';
             }
 
              $archivoController='controllers/'.$url[0].'.php';
-            if(file_exists($archivoController)){
+           if(file_exists($archivoController)){
               require_once $archivoController;
                 $controller=new $url[0];
                // $controller->render();
                 $controller->loadModel($url[0]);
-                if(isset($url[1])){
-                    $controller->{$url[1]}();  
-                
+                // # elementos del arreglo
+                $nparam = sizeof($url);
+                if($nparam > 1){
+                    if($nparam > 2){
+                        $param = [];
+                        for($i = 2; $i<$nparam; $i++){
+                            array_push($param, $url[$i]);
+                        }
+                        $controller->{$url[1]}($param);
+                    }else{
+                        $controller->{$url[1]}();
+                    }
                 }else{
                     $controller->render();
-
                 }
                
             }
