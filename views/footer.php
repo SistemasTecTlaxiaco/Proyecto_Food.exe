1cfcc3089cee1ddfb94f7304a5ac0793683806f8
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-17 15:24:42 -0600

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    
-    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/login.css"  type="text/css">
+    <link rel="stylesheet" href="<?php echo constant('URL');?>public/css/estilos.css"  type="text/css">
     <title></title>
 </head>
 <body>
