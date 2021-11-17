856d1b67db86cbd6411fc1203891d05bc719ff49
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-17 13:04:06 -0600

    Update errores.php

--- establecimientos/views/errores.php
+++ establecimientos/views/errores.php
@@ -8,11 +8,12 @@
     </head>
     <body>
     <?php require 'views/header.php';?>
-       <img  id="e" src="<?php echo constant('URL');?>public/IMG/error3.svg" alt="descripción" />
+     <img  id="e" src="<?php echo constant('URL');?>public/IMG/error3.svg" alt="descripción" />
 
         <div  id="error" class="error">
             <h1 class="error"><?php echo $this->mensaje; ?></h1>
         </div>
+        
    </body>
 
 </html>
