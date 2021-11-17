b00256e6a5175ef4245877552e51d2132e78658e
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-17 16:26:53 -0600

    Update database.php

--- libs/database.php
+++ libs/database.php
@@ -18,9 +18,9 @@ class  Database {
     }
     function  connect () {
         prueba {
-             $ dns = 'mysql: host =pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; puerto = 3306; nombrebd = gdxzaewzup8sigpm ' ;
-$ nombre de usuario = 'nj4v3bybsyrznops' ;
-$ contraseña = 'kcaoyaygqjrt2ry9' ;         
+             $ dns = 'mysql: host =w3epjhex7h2ccjxx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; puerto = 3306; nombrebd = wk1jkjwcw7iwluww ' ;
+$ nombre de usuario = 'oqvhc6hm3krmmi6u' ;
+$ contraseña = 'tukonndciuyimuzi' ;         
 $ opciones = matriz (
     PDO :: MYSQL_ATTR_INIT_COMMAND => "ESTABLECER NOMBRES 'utf8mb4'" ,
     PDO :: ATTR_DEFAULT_FETCH_MODE => PDO :: FETCH_OBJ );
