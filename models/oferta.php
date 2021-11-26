8df21d0ce4d63945240b40c7b58b7965e68f44e9
dulce-blm <72100538+dulce-blm@users.noreply.github.com>
2021-11-26 07:47:09 -0600

    Modelo de Oferta

---
+++ models/oferta.php
@@ -0,0 +1,12 @@
+<?php
+class Oferta{
+    public $id_ofertas;
+    public $nombre;
+    public $especificacion;
+    public $prec_anterior;
+    public $descuento;
+    public $id_establecimiento;
+
+
+}
+?>
