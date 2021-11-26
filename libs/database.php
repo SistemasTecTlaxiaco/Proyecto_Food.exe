8912dbfc3acd457365c6f62a4f530d65dfa66662
dulce-blm <72100538+dulce-blm@users.noreply.github.com>
2021-11-26 10:46:36 -0600

    Update database.php

--- libs/database.php
+++ libs/database.php
@@ -1,34 +1,29 @@
-<? php
-class  Database {
-     $ host privado ;
-    private  $ db ;
-     usuario $ privado ;
-     contraseña $ privada ;
-     juego de caracteres $ privado ;
-     puerto $ privado ;
+<?php
+class Database{
 
-     función  pública __construct () {
-        $ esto -> host = constante ( 'HOST' );
-        $ esto -> db = constante ( 'DB' );
-        $ esto -> usuario = constante ( 'USUARIO' );
-        $ esto -> contraseña = constante ( 'CONTRASEÑA' );
-        $ this -> charset = constante ( 'CHARSET' );
-        $ esto -> puerto = constante ( 'PUERTO' );
+    function connect(){
+        try{
+        //DATOS PARA CONEXION HEROKU
+        $dsn = 'mysql:host=yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; port=3306; dbname=xhkiyv5mw3pakpgk;';
+        $username = 'x4k7hfjbkl5nckf8';
+        $password = 'lgwazuu5q3zpctgl'; 
+        
+        /*PARA PRUEBAS LOCALES*/
+        //$dsn = 'mysql:host=localhost; port=3306; dbname=food;';
+        //El usuario por defecto es ROOT, sin contraseña    
+        //$username = 'root';
+        //$password = '';
+ 
+        $options = array(
+   //La extensión Objetos de Datos de PHP (PDO) 
+   //define un interfaz ligera para poder acceder a cualquier base de datos en PHP .
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ);
+    $pdo = new PDO($dsn, $username, $password, $options);
+    return $pdo;
 
-    }
-    function  connect () {
-        prueba {
-             $ dns = 'mysql: host =w3epjhex7h2ccjxx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; puerto = 3306; nombrebd = wk1jkjwcw7iwluww ' ;
-$ nombre de usuario = 'oqvhc6hm3krmmi6u' ;
-$ contraseña = 'tukonndciuyimuzi' ;         
-$ opciones = matriz (
-    PDO :: MYSQL_ATTR_INIT_COMMAND => "ESTABLECER NOMBRES 'utf8mb4'" ,
-    PDO :: ATTR_DEFAULT_FETCH_MODE => PDO :: FETCH_OBJ );
-    $ pdo = nuevo  PDO ( $ dsn , $ nombre de usuario , $ contraseña , $ opciones );
-    return  $ pdo ;
-
-        } captura ( PDOException  $ e ) {
-            print_r ( 'Error de conexión:' . $ e -> getMessage ());
+        }catch (PDOException $e){
+            print_r('Error connection: '.$e->getMessage());
         }        
     }
 }
