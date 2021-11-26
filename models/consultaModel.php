812fade7b2300ed691d392694030fb5229d4b416
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-25 23:01:02 -0600

    edicion datos establecimiento

--- models/consultaModel.php
+++ models/consultaModel.php
@@ -12,6 +12,47 @@ include_once 'models/platillo.php';
        public function get(){
       
        }
+      
+       public function getById($id){
+        $items=[];
+
+        try{
+            
+            $query=$this->db->connect()->query("SELECT * FROM establecimiento WHERE id_establecimiento='$id'");
+            while($row=$query->fetch((PDO::FETCH_ASSOC))){
+                $item=new establecimiento();
+                $item->nombre=$row['nombre'];
+                $item->telefono=$row['telefono'];
+                $item->horario=$row['horario'];
+                $item->ubicacion=$row['ubicacion'];
+                $item->doc_valida=$row['doc_valida'];
+                $item->contra=$row['contra'];
+                $item->id_establecimiento=$row['id_establecimiento'];
+
+                array_push($items,$item);
+            }
+            return $items;
+        }catch(PDOException $e){
+            return[];
+        }
+       }
+       
+       public function update($item){
+        $query = $this->db->connect()->prepare("UPDATE establecimiento SET nombre = :nombre, telefono = :telefono, horario = :horario, ubicacion = :ubicacion, contra = :contra WHERE id_establecimiento = :id_establecimiento");
+        try{
+            $query->execute([ 
+                'id_establecimiento'=> $item['id_establecimiento'],
+                'nombre'=> $item['nombre'],
+                'telefono'=> $item['telefono'],
+                'horario'=>$item['horario'],
+                'ubicacion'=>$item['ubicacion'],
+                'contra'=>$item['contra']
+            ]);
+            return true;
+        }catch(PDOException $e){
+            return false; 
+        }
+       }
    }
 
 ?>
