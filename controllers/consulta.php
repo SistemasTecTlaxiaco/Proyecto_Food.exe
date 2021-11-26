f8059605a1ed2b4120d4bbfb94b525996657be35
Nancy2050 <64548201+Nancy2050@users.noreply.github.com>
2021-11-25 22:51:32 -0600

    editar establecim

--- controllers/consulta.php
+++ controllers/consulta.php
@@ -12,6 +12,44 @@
            $this->view->establecimiento=$establecimiento;
            $this->view->render('home_establecimiento');
        }
+      
+       function editarEstablecimiento($param=null){
+           
+        $id_establecimiento = $param[0];
+        $establecimiento = $this->model->getById($id_establecimiento);
+        
+        $this->view->establecimiento = $establecimiento;
+        $this->view->mensaje = "";
+        $this->view->render('modificar_establecimiento');
+    }
+    function actualizarEstablecimiento(){
+        
+        $id_establecimiento=$_POST['id_establecimiento'];
+        $nombre=$_POST['nombre'];
+        $ubicacion  = $_POST['ubicacion'];
+        $telefono=$_POST['telefono'];
+        $contra=$_POST['contra'];
+        $horario=$_POST['horario'];
+
+
+        if($this->model->update(
+            ['id_establecimiento'=>$id_establecimiento,'nombre' => $nombre, 
+            'ubicacion' => $ubicacion,'telefono' => $telefono,'contra'=> $contra, 'horario'=>$horario] )){
+            // actualizar platillo exito
+            $establecimiento = new establecimiento();
+            $establecimiento->id_establecimiento = $id_establecimiento;
+            $establecimiento->nombre = $nombre;
+            $establecimiento->ubicacion = $ubicacion;
+            $establecimiento->telefono = $telefono;
+            $establecimiento->contra = $contra;
+
+            $this->view->establecimiento = $establecimiento;
+        }else{
+            // mensaje de error
+            echo "No se pudo actualizar el establecimiento";
+        }
+        $this->view->render('home_establecimiento');
+    }
 
    }
 
