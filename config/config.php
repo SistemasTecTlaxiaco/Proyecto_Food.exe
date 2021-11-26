ed502ba2e153c56ad9b1d99979dada2a56d9bd8d
dulce-blm <72100538+dulce-blm@users.noreply.github.com>
2021-11-26 12:13:01 -0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 //mantiene estructura sin importar que parte del documento se acceda
-define ('URL','https://foodexe2.herokuapp.com/');
-
+define ('URL','https://fo0exe.herokuapp.com/');
+//define ('URL','https://localhost/Proyecto/');
 //variables para la base de datos
-define ('DB_HOST','pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define ('DB_NAME','gdxzaewzup8sigpm');
-define ('DB_USER','nj4v3bybsyrznops');
-define ('DB_PASSWORD','kcaoyaygqjrt2ry9');
-define ('DB_CHARSET','utf8');
-//define ('PUERTO','3306');
+define ('HOST','yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define ('DB','xhkiyv5mw3pakpgk');
+define ('USER','x4k7hfjbkl5nckf8');
+define ('PASSWORD','lgwazuu5q3zpctgl');
+define ('CHARSET','utf8mb4');
+define ('PUERTO','3306');
 ?>
