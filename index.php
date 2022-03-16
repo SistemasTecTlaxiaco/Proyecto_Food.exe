d73d3312f5c72c93e85a9ab714e3110ac31dd9a2
SANTIAGO <64548201+Nancy2050@users.noreply.github.com>
2022-03-16 01:36:23 -0600

    MENU PRINCIPAL PRUEBA

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html>  
   <head>
-    <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script> 
-    <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
-   <!-- estamos llamando a las dos librerías de Aframe y ARJS-->
+  <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PRODUCTOS</title>
   </head>   
 
   <body >
-    <a-scene embedded arjs> <!--Scene embedded será el lanzador de la realidad aumentada-->
-      <a-entity scale=".2 .2 .2"> <!--a-entity encapsulará el archivo 3D-->
-        <a-entity gltf-model="pizzara.glb" scale="1 1 1" crossOrigin="anonymous">
-        </a-entity>
-        <a-marker-camera preset='hiro'></a-marker-camera><!--marker-camera es el llamado a la cámara-->
-        </a-scene>
-      </body>
-    </html>
+ 
+   <div class="login-container">
+        <div class="login-information-container">
+            <h1 class="title">PRODUCTOS</h1>
+            <a href="pizza.php" class="btn"><button class="btn">Pizza</button></a>
+            <a href="hamburguesa.php" class="btn"><button class="btn">Hamburguesa</button></a>
+            <a href="sandwich.php" class="btn"><button class="btn">Sandwich</button></a>
+            <a href="jugo.php" class="btn"><button class="btn">Jugo</button></a>
+            <a href="refresco.php" class="btn"><button class="btn">Refresco</button></a>
+            <a href="cafe.php" class="btn"><button class="btn">Cafe</button></a>
+            <a href="helado.php" class="btn"><button class="btn">Helado</button></a>
+            <a href="gelatina.php" class="btn"><button class="btn">Gelatina</button></a>
+            <a href="pastel.php" class="btn"><button class="btn">Pastel</button></a>
+
+
+        </div>
+        <img class="image-container" src="mujer.png" alt="login-image-lateral">
+    </div>
+  </body>
+</html>
