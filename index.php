dc23d18889073c2015a96780b5f7fd48385824b3
SANTIAGO <64548201+Nancy2050@users.noreply.github.com>
2022-03-15 22:27:03 -0600

    prueba ra

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<?php
-    require_once 'libs/database.php';
-    require_once 'libs/controller.php';
-    require_once 'libs/view.php';
-    require_once 'libs/model.php';
-    require_once 'libs/app.php';
-    require_once 'config/config.php';
+<!DOCTYPE html>
+<html>  
+  <head>
+    <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script> 
+    <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+   <!-- estamos llamando a las dos librerías de Aframe y ARJS-->
+  </head>   
 
-    $app=new App();
-?>
+  <body>
+    <a-scene embedded arjs> <!--Scene embedded será el lanzador de la realidad aumentada-->
+      <a-entity scale=".2 .2 .2"> <!--a-entity encapsulará el archivo 3D-->
+        <a-entity gltf-model="Hamburguesin.glb" scale="3 3 3" crossOrigin="anonymous">
+            <!--Crossorigin para evitar problemas del navegador-->
+        </a-entity>
+        <a-marker-camera preset='hiro'></a-marker-camera><!--marker-camera es el llamado a la cámara-->
+        </a-scene>
+      </body>
+    </html>
