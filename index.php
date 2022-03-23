8659cb201e19ae17f785d2e27bed212d20800f78
SANTIAGO <64548201+Nancy2050@users.noreply.github.com>
2022-03-23 11:40:50 -0600

    animacion

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
             <a href="helado.php" class="btn"><button class="btn">Helado</button></a>
             <a href="gelatina.php" class="btn"><button class="btn">Gelatina</button></a>
             <a href="pastel.php" class="btn"><button class="btn">Pastel</button></a>
+            <a href="animación" class="btn"><button class="btn">Animación promocional</button></a>
 
 
         </div>
