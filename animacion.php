70ce837bae34f109533a24d3d07b3315a64377b9
SANTIAGO <64548201+Nancy2050@users.noreply.github.com>
2022-04-04 08:51:27 -0500

    Update animacion.php

--- animacion.php
+++ animacion.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html>  
   <head>
-    <script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script> 
-    <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
-   <!-- estamos llamando a las dos librerías de Aframe y ARJS-->
+     <!-- Import the component -->
+<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+
   </head>   
 
   <body >
-    <a-scene embedded arjs> <!--Scene embedded será el lanzador de la realidad aumentada-->
-      <a-entity scale=".2 .2 .2"> <!--a-entity encapsulará el archivo 3D-->
-        <a-entity gltf-model="hot-dog.glb" scale="1 1 1" crossOrigin="anonymous">
-        </a-entity>
-        <a-marker-camera preset='hiro'></a-marker-camera><!--marker-camera es el llamado a la cámara-->
-        </a-scene>
+
+<!-- Use it like any other HTML element -->
+<model-viewer alt="mesero"
+ src="mesero.glb" 
+ ar ar-modes="fixed" 
+ environment-image="" 
+ poster="" 
+ seamless-poster
+ amera-controls autoplay ar shadow-intensity="1"
+ enable-pan
+ auto rotate>
+</model-viewer>
       </body>
     </html>
